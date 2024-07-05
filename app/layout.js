9fc77adffc5a13746b1353f0a47ab8d1d52cf49a
MaxCodeCraft <maxcodecraft06@gmail.com>
2024-07-05 08:24:26 +0200

    new logo

--- app/layout.js
+++ app/layout.js
@@ -6,7 +6,7 @@ import "./globals.css";
 const offside = Offside({ subsets: ["latin"], weight: "400" });
 
 export const metadata = {
-  title: "Maxime Bocquet - Portfolio",
+  title: "MaxCodeCraft | Maxime Bocquet",
   description:
     "Je suis un développeur web full stack passionné avec une expérience unique dans la vente. J'utilise la stack MERN et j'apprécie les bibliothèques React telles que Tailwind CSS et Framer Motion. Grâce à un bootcamp intensif, j'ai maîtrisé les frameworks nécessaires pour donner vie à vos projets !",
 };
